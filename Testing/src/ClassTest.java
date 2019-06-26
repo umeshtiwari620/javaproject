8a967a680b46ac5f26093c5e6e4ca7cb3c36f510
umtiwari <umtiwari@DIN60016052.corp.capgemini.com>
2019-06-26 11:52:31 +0530

    Commited Second Changes

--- Testing/src/ClassTest.java
+++ Testing/src/ClassTest.java
@@ -5,6 +5,10 @@ public class ClassTest {
 		// TODO Auto-generated method stub
 		System.out.println("Hi I am Testing The Project");
 
+		fun();
 	}
 
+	public static void fun() {
+		System.out.println("Created Function Function");
+	}
 }
